78e9db8b1d5432c1b3f1504ce24918fd2c64ee0c
Shreya Tiwari <210050146@iitb.ac.in>
2025-10-18 16:20:49 +0530

    started making the non-trivial tau function

--- milp_solver/algorithms.py
+++ milp_solver/algorithms.py
@@ -11,4 +11,8 @@ from pareto_points import Pareto_Points
 class non_trivial_tau_set:
     def __init__(self, dir , max_nodes ):
         self.inp = Input(dir, max_nodes)
-        
+        self.pareto_points = []
+        self.max_nodes = max_nodes
+
+    def non_trivial_tau_set(self):
+        self.pareto_points.append()
